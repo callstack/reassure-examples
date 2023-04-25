26e7a49cc37c99db00773490f4d14306907cbee2
josemak25 <amakirij@gmail.com>
2023-04-25 16:45:01 +0100

    add danger file path fix

--- dangerfile.js
+++ dangerfile.js
@@ -3,8 +3,5 @@ const path = require('node:path');
 const { dangerReassure } = require('@callstack/reassure-danger');
 
 dangerReassure({
-  inputFilePath: path.join(__dirname, './examples/native/.reassure/output.md'),
-  inputFilePath: path.join(__dirname, './examples/web-vite/.reassure/output.md'),
-  inputFilePath: path.join(__dirname, './examples/web-nextjs/.reassure/output.md'),
-  inputFilePath: path.join(__dirname, './examples/native-expo/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './reassure/output.md'),
 });
