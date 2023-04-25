699e707fb7573ffc68d29a3e0c6161601d8f8870
josemak25 <amakirij@gmail.com>
2023-04-25 17:37:47 +0100

    add dangerfile to other test output files

--- dangerfile.js
+++ dangerfile.js
@@ -2,5 +2,8 @@ const path = require('node:path');
 const { dangerReassure } = require('@callstack/reassure-danger');
 
 dangerReassure({
-  inputFilePath: path.join(__dirname, './reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/native/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/web-vite/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/web-nextjs/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/native-expo/.reassure/output.md'),
 });
