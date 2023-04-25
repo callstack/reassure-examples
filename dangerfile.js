11d4e79d1abf3ea24c712210ae6b3c6f34ca0bff
josemak25 <amakirij@gmail.com>
2023-04-25 14:32:19 +0100

    add example apps output to dangerReassure file

--- dangerfile.js
+++ dangerfile.js
@@ -4,4 +4,7 @@ import { dangerReassure } from 'reassure';
 
 dangerReassure({
   inputFilePath: path.join(__dirname, './examples/native/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/native-expo/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/web-nextjs/.reassure/output.md'),
+  inputFilePath: path.join(__dirname, './examples/web-vite/.reassure/output.md'),
 });
