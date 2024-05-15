64709e12b4939a45acf4c80695dc2a2a461fa8b6
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-05-15 22:56:33 +0200

    chore: re-create example from CLI

--- examples/native/android/app/src/main/java/com/reassurenativeexample/MainActivity.kt
+++ examples/native/android/app/src/main/java/com/reassurenativeexample/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.reassurereactnativeexample
+package com.reassurenativeexample
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "ReassureReactNativeExample"
+  override fun getMainComponentName(): String = "ReassureNativeExample"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
