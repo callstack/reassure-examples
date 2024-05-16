372ad84c798a797cd32c87e6f1dacf078cbb1460
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-05-16 15:58:31 +0200

    chore: danger 4x

--- dangerfile.js
+++ dangerfile.js
@@ -4,3 +4,15 @@ const { dangerReassure } = require('@callstack/reassure-danger');
 dangerReassure({
   inputFilePath: path.join(__dirname, './examples/native/.reassure/output.md'),
 });
+
+dangerReassure({
+  inputFilePath: path.join(__dirname, './examples/native-expo/.reassure/output.md'),
+});
+
+dangerReassure({
+  inputFilePath: path.join(__dirname, './examples/web-vite/.reassure/output.md'),
+});
+
+dangerReassure({
+  inputFilePath: path.join(__dirname, './examples/web-nextjs/.reassure/output.md'),
+});
