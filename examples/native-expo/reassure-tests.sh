2dacb0599dc6eb7a0ee8f08cbf5c160b47b03e3c
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-05-16 16:38:42 +0200

    chore: update reassure scripts

--- examples/native-expo/reassure-tests.sh
+++ examples/native-expo/reassure-tests.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
+set -e 
+
+BASELINE_BRANCH=${GITHUB_BASE_REF:="main"}
+
+# Required for `git switch` on CI
+git fetch origin
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
+
 yarn install
 yarn reassure --baseline
 
 # Gather current perf measurements & compare results
-git switch -
+git switch --detach -
+
 yarn install
 yarn reassure --branch
