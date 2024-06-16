6a409798811a1165aa1d094bfd35cb88448c24a6
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-06-16 22:24:58 +0200

    chore: react compiler setup

--- examples/native-cli/babel.config.js
+++ examples/native-cli/babel.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+const ReactCompilerConfig = {
+  runtimeModule: 'react-compiler-runtime'
+};
+
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      ['babel-plugin-react-compiler', ReactCompilerConfig], // must run first!
+      // ...
+    ],
+  };
 };
--- examples/native-cli/src/SlowList.test.tsx
+++ examples/native-cli/src/SlowList.test.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { render, screen } from '@testing-library/react-native';
 import { expect, test } from '@jest/globals';
 import { SlowList } from './SlowList';
