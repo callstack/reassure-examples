27bb8c5ab15b14bb70c9467b1711425291247758
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-03-06 18:43:55 +0100

    .

--- examples/native-expo/jest-setup.js
+++ examples/native-expo/jest-setup.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-undef */
 import { configure } from 'reassure';
 
-configure({ testingLibrary: 'react-native' });
-
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+configure({ testingLibrary: 'react-native', dropOutliers: true });
