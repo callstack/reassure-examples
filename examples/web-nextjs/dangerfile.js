7767348bc214901126918fba13166640633d2f9b
Fábio Henriques <contact@fabiohenriques.dev>
2025-05-20 18:13:09 +0100

    fix: expose __dirname to dangerjs

--- examples/web-nextjs/dangerfile.js
+++ examples/web-nextjs/dangerfile.js
@@ -1,6 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
+import { fileURLToPath } from "url";
 import { dangerReassure } from 'reassure';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dangerReassure({
   inputFilePath: path.join(__dirname, './.reassure/output.md'),
