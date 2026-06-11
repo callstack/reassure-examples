6e48c14454b74ea77686d34b238b2a9c3cb0c91f
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-06-11 11:38:55 +0200

    fix danger

--- examples/native-expo/dangerfile.js
+++ examples/native-expo/dangerfile.js
@@ -1,6 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { dangerReassure } from 'reassure';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 dangerReassure({
   inputFilePath: path.join(__dirname, './.reassure/output.md'),
