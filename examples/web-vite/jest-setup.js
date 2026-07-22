a2e0fbb05f6260645f500e3b431c9c48c2d780e1
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2026-07-22 10:04:53 +0200

    chore: reassure v1.6.0

--- examples/web-vite/jest-setup.js
+++ examples/web-vite/jest-setup.js
@@ -1,7 +1,3 @@
 import { configure } from 'reassure';
-import { configure as configureRtc } from 'react-test-config';
 
 configure({ testingLibrary: 'react' });
-configureRtc({
-  enableOwnerStacks: false,
-});
